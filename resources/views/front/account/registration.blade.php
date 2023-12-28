b0ed0c9d23e990b7c9aee4cdea1fef4e7271f723
ruchimp010 <93474879+ruchimp010@users.noreply.github.com>
2023-12-28 10:53:07 +0530

    registration

--- resources/views/front/account/registration.blade.php
+++ resources/views/front/account/registration.blade.php
@@ -12,25 +12,25 @@
                             <div class="mb-3">
                                 <label for="" class="mb-2">Name*</label>
                                 <input type="text" name="name" id="name" class="form-control"
-                                    placeholder="Enter Name" >
+                                    placeholder="Enter Name">
                                 <p></p>
                             </div>
                             <div class="mb-3">
                                 <label for="" class="mb-2">Email*</label>
                                 <input type="text" name="email" id="email" class="form-control"
-                                    placeholder="Enter Email" >
+                                    placeholder="Enter Email">
                                 <p></p>
                             </div>
                             <div class="mb-3">
                                 <label for="" class="mb-2">Password*</label>
                                 <input type="password" name="password" id="password" class="form-control"
-                                    placeholder="Enter Password" >
+                                    placeholder="Enter Password">
                                 <p></p>
                             </div>
                             <div class="mb-3">
                                 <label for="" class="mb-2">Confirm Password*</label>
-                                <input type="confirm_password" name="confirmpassword" id="name" class="form-control"
-                                    placeholder="please confirm Password" >
+                                <input type="password" name="confirm_password" id="confirm_password" class="form-control"
+                                    placeholder="please confirm Password">
                             </div>
                             <button class="btn btn-primary mt-2">Register</button>
                         </form>
@@ -62,53 +62,47 @@
                                 .addClass('invalid-feedback')
                                 .html(errors.name)
 
-                        }else{
-                            $("#name").addClass('is.invalid')
+                        } else {
+                            $("#name").removeClass('is.invalid')
                                 .siblings('p')
-                                .addClass('invalid-feedback')
+                                .removeClass('invalid-feedback')
                                 .html('')
                         }
-                    }
-                    if (response.status == false) {
-                        var errors = response.errors;
+
                         if (errors.email) {
                             $("#email").addClass('is.invalid')
                                 .siblings('p')
                                 .addClass('invalid-feedback')
                                 .html(errors.email)
 
-                        }else{
+                        } else {
                             $("#email").addClass('is.invalid')
                                 .siblings('p')
                                 .addClass('invalid-feedback')
                                 .html('')
                         }
-                    }
-                    if (response.status == false) {
-                        var errors = response.errors;
+
                         if (errors.password) {
                             $("#password").addClass('is.invalid')
                                 .siblings('p')
                                 .addClass('invalid-feedback')
                                 .html(errors.password)
 
-                        }else{
+                        } else {
                             $("#password").addClass('is.invalid')
                                 .siblings('p')
                                 .addClass('invalid-feedback')
                                 .html('')
                         }
-                    }
-                    if (response.status == false) {
-                        var errors = response.errors;
-                        if (errors.confirmpassword) {
-                            $("#name").addClass('is.invalid')
+
+                        if (errors.confirm_password) {
+                            $("#confirm_password").addClass('is.invalid')
                                 .siblings('p')
                                 .addClass('invalid-feedback')
                                 .html(errors.password)
 
-                        }else{
-                            $("#confirmpassword").addClass('is.invalid')
+                        } else {
+                            $("#confirm_password").addClass('is.invalid')
                                 .siblings('p')
                                 .addClass('invalid-feedback')
                                 .html('')
@@ -116,9 +110,11 @@
                     }
 
 
+                }else{
+
                 }
 
-            })
+            });
 
         });
     </script>
